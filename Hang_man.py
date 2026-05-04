130fe6e071aeec2da8d3b8ce023b94818902845b
Anaya_Abro <abroayaan8@gmail.com>
2026-05-04 21:32:56 +0500

    Add files via upload

---
+++ Hang_man.py
@@ -0,0 +1,32 @@
+
+import random
+print('Hangman Game')
+
+secret = ['Kingdom','beautiful','python','laptop','computer','science','apple','mother',]
+computer = random.choice(secret).lower()
+display = ['_']*len(computer)
+
+lives = 5
+guessed = []
+while lives>0:
+    print(display)
+    print('letter guessed so far:',guessed)
+    user = input('guess a letter:').lower()
+    guessed.append(user)
+
+
+    if user in computer:
+        for i in range(len(computer)):
+            if computer[i] == user:
+                display[i] = user
+                
+    else:
+        lives-=1
+        print(f'wrong! remaining lives: {lives}')    
+
+    if '_' not in display:
+        print('you win🎉')
+        break
+
+if lives == 0:
+    print('Game Over! the word was',computer)
